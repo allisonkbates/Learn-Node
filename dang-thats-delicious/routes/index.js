13cfb03d6d208568bb6ffc8290a82792375647bc
Allison Bates <allisonkbates@gmail.com>
2018-10-21 14:39:42 -0400

    test

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -11,19 +11,16 @@ router.get('/add',
 	authController.isLoggedIn, 
 	storeController.addStore
 );
-
 router.post('/add', 
 	storeController.upload,
 	catchErrors(storeController.resize),
 	catchErrors(storeController.createStore)
 );
-
 router.post('/add/:id', 
 	storeController.upload,
 	catchErrors(storeController.resize),
 	catchErrors(storeController.updateStore)
 	);
-
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
@@ -33,7 +30,6 @@ router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 router.get('/login', userController.loginForm);
 router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
-
 router.post('/register', 
 	userController.validateRegister,
 	userController.register,
