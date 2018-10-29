8b92fd7b829554f77daa715be0b95786571c2045
Allison Bates <allisonkbates@gmail.com>
2018-10-29 18:57:02 -0400

    video 33

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -118,6 +118,23 @@ exports.searchStores = async (req, res) => {
 	// limit to only 5 results
 	.limit(5);
 	res.json(stores);
-}
+};
+
+exports.mapStores = async (req, res) => {
+	const coordinates = [req.query.lng, req.query.lat].map(parseFloat);
+	const q = {
+		location: {
+			$near: {
+				$geometry: {
+					type: 'Point',
+					coordinates: coordinates
+				},
+				$maxDistance: 10000 // 10km
+			}
+		}
+	}
+const stores = await Store.find(q).select('slug name description location').limit(10);
+res.json(stores);
+};
 
 
