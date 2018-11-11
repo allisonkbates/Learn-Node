0edfd213e73249937cc419803d393c799420f3b3
Allison Bates <allisonkbates@gmail.com>
2018-11-10 23:46:31 -0500

    video 38

--- dang-thats-delicious/models/Store.js
+++ dang-thats-delicious/models/Store.js
@@ -38,6 +38,9 @@ const storeSchema = new mongoose.Schema({
 		ref: 'User',
 		required: 'You must supply an author!'
 	}
+}, {
+	toJSON: { virtuals: true },
+	toObject: { virtuals: true }
 });
 
 //define our indexes
@@ -72,6 +75,12 @@ storeSchema.statics.getTagsList = function() {
 		{ $group: { _id: '$tags', count: { $sum: 1} }},
 		{ $sort: {count: -1}}
 	]);
-}
+};
+
+storeSchema.virtual('reviews', {
+	ref: 'Review',
+	localField: '_id', // which field on the store model?
+	foreignField: 'store' // which field on the review model?
+});
 
 module.exports = mongoose.model('Store', storeSchema);
